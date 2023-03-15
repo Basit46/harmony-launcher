8de4e1c81dd0caf8ecc61fc519a2b7a7887267e6
Basit46 <Hassanbasit1990@gmail.com>
2023-03-15 18:23:51 +0100

    Added changes

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -5,7 +5,7 @@ const LeftNav = () => {
   return (
     <div className="left-nav fixed top-[18vh] w-[20vw] left-0 h-[82vh] p-5">
       <div className="flex flex-col items-center">
-        <ul className="w-fit h-full flex flex-col space-y-[55px]">
+        <ul className="w-fit h-full flex flex-col space-y-[45px]">
           <NavLink to="/">Home</NavLink>
           <NavLink to="/usecases">Use Cases</NavLink>
           <NavLink to="/features">Features</NavLink>
