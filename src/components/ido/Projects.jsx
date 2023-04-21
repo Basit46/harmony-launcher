ad3180819b5c49892ffc744cf62f9085e7df1d11
Basit46 <Hassanbasit1990@gmail.com>
2023-04-21 17:57:25 +0100

    Added changes

--- src/components/ido/Projects.jsx
+++ src/components/ido/Projects.jsx
@@ -23,26 +23,28 @@ const Projects = () => {
         <li>Avalanche</li>
       </ul>
 
-      <div className="mt-[35px] h-[350px] overflow-x-hidden overflow-y-au">
-        <table className="table-auto h-fit">
-          <thead>
-            <tr>
-              <th className="">Project name</th>
-              <th className="">Type</th>
-              <th>Participants</th>
-              <th>Total name</th>
-              <th>Current price</th>
-              <th>All price IDO</th>
-              <th className="">Ended in</th>
-              <th className="">Chains</th>
-            </tr>
-          </thead>
-          <tbody>
-            <Project />
-            <Project />
-            <Project />
-          </tbody>
-        </table>
+      <div className="mt-[35px] h-fit overflow-x-auto w-[85vw]">
+        <div className="w-fit bg-[green] ">
+          <table className="table-auto h-fit ">
+            <thead>
+              <tr>
+                <th className="">Project name</th>
+                <th className="">Type</th>
+                <th>Participants</th>
+                <th>Total name</th>
+                <th>Current price</th>
+                <th>All price IDO</th>
+                <th className="">Ended in</th>
+                <th className="">Chains</th>
+              </tr>
+            </thead>
+            <tbody>
+              <Project />
+              <Project />
+              <Project />
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
